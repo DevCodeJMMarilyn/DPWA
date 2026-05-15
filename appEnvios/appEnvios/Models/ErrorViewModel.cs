03d0047b3bb66c7200cdfb17605d26170e445c14
Marilyn <jimenezm.m2021@gmail.com>
2026-05-15 16:24:59 -0600

    update

--- appEnvios/appEnvios/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace appEnvios.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
